e4e4e99d890eef4b3bd86528642bec4723f4a61b
Imran-S-heikh <imransheikhsadi@gmail.com>
2023-11-20 08:47:31 +0600

    Config Secure True

--- src/libs/email.ts
+++ src/libs/email.ts
@@ -20,7 +20,7 @@ export async function sendMail({ from, name, subject, text }: MailOptions) {
   const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: false,
+    secure: true,
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASS,
