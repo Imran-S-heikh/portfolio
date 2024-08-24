0bf6b4cc4f444508b10f408e1d1b204e0d5c33c6
Imran-s-heikh <imransheikhsadi@gmail.com>
2024-08-25 02:02:49 +0600

    feat: Include Uddogta As Project

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -3,14 +3,14 @@ import { RepoIds } from "./repo-ids";
 import { GET_REPOSITORIES_BY_ID } from "@dev/libs/query";
 import { Repository } from "./types";
 
-const { PORTFOLIO, RETOMIZER, DEVFINDER } = RepoIds;
+const { PORTFOLIO, RETOMIZER, DEVFINDER, UDDOGTA } = RepoIds;
 
 export async function getGithubProjects() {
   const octokit = new Octokit({
     auth: process.env.GITHUB_AUTH_TOKEN,
   });
   const data = (await octokit.graphql(GET_REPOSITORIES_BY_ID, {
-    repoIds: [PORTFOLIO, RETOMIZER, DEVFINDER],
+    repoIds: [PORTFOLIO, RETOMIZER, DEVFINDER, UDDOGTA],
   })) as { nodes: Repository[] };
 
   return data.nodes;
