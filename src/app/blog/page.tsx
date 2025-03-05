126eb1f5d770826cad6bd77d1df14a8a1e3f5b70
Imran-S-heikh <imransheikhsadi@gmail.com>
2025-03-05 18:07:11 +0600

    fix: Blog Mobile Responsive

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -38,7 +38,7 @@ function BlogPage() {
   const posts = [reactSpecialist];
   return (
     <main className="flex-1">
-      <div className="grid grid-cols-4 gap-x-5 gap-y-10 m-5">
+      <div className="grid lg:grid-cols-3 xl:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-x-5 gap-y-10 m-5">
         {posts.map((post, index) => (
           <BlogPreviewCard key={index} {...post} />
         ))}
