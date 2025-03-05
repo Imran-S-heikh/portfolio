d45f7827c3cbf2cc5dd02e503083c10779ab99c1
Imran-S-heikh <imransheikhsadi@gmail.com>
2025-03-05 18:53:50 +0600

    fix: Padding And Margion Blog Page

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from "react";
 
 function Layout({ children }: { children: ReactNode }) {
-  return <div className="lg:m-4  flex-1">{children}</div>;
+  return <div className="lg:m-10 m-4  flex-1">{children}</div>;
 }
 
 export default Layout;
